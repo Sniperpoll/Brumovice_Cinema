9f811774e96c665ef26b04f71f4557755765181b
Sniperpoll <sniperpoll@gmail.com>
2025-05-20 00:31:21 +0200

    v0.0.1

--- plugin.video.brumovicecinema/default.py
+++ plugin.video.brumovicecinema/default.py
@@ -1,23 +1,40 @@
 import sys
+import urllib.parse
 import xbmcplugin
 import xbmcgui
 import xbmcaddon
+import requests
 
 # Inicializace
 addon = xbmcaddon.Addon()
 addon_handle = int(sys.argv[1])
 base_url = sys.argv[0]
+params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
 
 # Získání přihlašovacích údajů
 username = addon.getSetting('ws_username')
 password = addon.getSetting('ws_password')
 
-# Vytvoření základního menu
+# Přihlášení k Webshare API
+def login_to_webshare():
+    url = 'https://webshare.cz/api/login/'
+    data = {'username': username, 'password': password}
+
+    try:
+        response = requests.post(url, data=data)
+        if response.status_code == 200 and 'token' in response.json():
+            token = response.json()['token']
+            xbmcgui.Dialog().notification('Brumovice Cinema', 'Přihlášení OK ✅', xbmcgui.NOTIFICATION_INFO, 3000)
+        else:
+            xbmcgui.Dialog().notification('Brumovice Cinema', 'Chyba přihlášení ❌', xbmcgui.NOTIFICATION_ERROR, 3000)
+    except Exception as e:
+        xbmcgui.Dialog().notification('Chyba', str(e), xbmcgui.NOTIFICATION_ERROR, 5000)
+
+# Základní menu
 def main_menu():
     xbmcplugin.setPluginCategory(addon_handle, 'Brumovice Cinema')
     xbmcplugin.setContent(addon_handle, 'videos')
 
-    # Přidáme položku "Ověřit přihlášení"
     li = xbmcgui.ListItem(label='🔐 Ověřit přihlášení')
     url = f'{base_url}?action=login'
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
@@ -26,4 +43,8 @@ def main_menu():
 
 # Hlavní vstup
 if __name__ == '__main__':
-    main_menu()
+    action = params.get('action')
+    if action == 'login':
+        login_to_webshare()
+    else:
+        main_menu()
