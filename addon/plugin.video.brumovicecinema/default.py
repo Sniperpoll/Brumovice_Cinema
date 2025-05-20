e8688cfc9087334fe21bd5be009037e19acdf433
Sniperpoll <sniperpoll@gmail.com>
2025-05-20 10:11:56 +0200

    v0.0.1

--- addon/plugin.video.brumovicecinema/default.py
+++ addon/plugin.video.brumovicecinema/default.py
@@ -2,8 +2,66 @@ import xbmcplugin
 import xbmcgui
 import xbmcaddon
 import sys
+import urllib.parse
 
 addon = xbmcaddon.Addon()
 handle = int(sys.argv[1])
+base_url = sys.argv[0]
 
-xbmcplugin.endOfDirectory(handle)
+def build_url(query):
+    return base_url + '?' + urllib.parse.urlencode(query)
+
+def main_menu():
+    xbmcplugin.addDirectoryItem(
+        handle,
+        build_url({"action": "search"}),
+        xbmcgui.ListItem(label="Vyhledávání"),
+        isFolder=True
+    )
+    xbmcplugin.addDirectoryItem(
+        handle,
+        build_url({"action": "movies"}),
+        xbmcgui.ListItem(label="Filmy"),
+        isFolder=True
+    )
+    xbmcplugin.addDirectoryItem(
+        handle,
+        build_url({"action": "tvshows"}),
+        xbmcgui.ListItem(label="Seriály"),
+        isFolder=True
+    )
+    xbmcplugin.addDirectoryItem(
+        handle,
+        build_url({"action": "tvguide"}),
+        xbmcgui.ListItem(label="TV Program"),
+        isFolder=True
+    )
+    xbmcplugin.addDirectoryItem(
+        handle,
+        build_url({"action": "trakt"}),
+        xbmcgui.ListItem(label="Trakt.tv"),
+        isFolder=True
+    )
+    xbmcplugin.addDirectoryItem(
+        handle,
+        build_url({"action": "tmdb"}),
+        xbmcgui.ListItem(label="TMDB"),
+        isFolder=True
+    )
+    xbmcplugin.addDirectoryItem(
+        handle,
+        build_url({"action": "csfd"}),
+        xbmcgui.ListItem(label="ČSFD"),
+        isFolder=True
+    )
+    xbmcplugin.addDirectoryItem(
+        handle,
+        build_url({"action": "settings"}),
+        xbmcgui.ListItem(label="Nastavení"),
+        isFolder=True
+    )
+
+    xbmcplugin.endOfDirectory(handle)
+
+if __name__ == '__main__':
+    main_menu()
